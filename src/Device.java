c05619e782991f03e1b1bbb84280ffee1844ceaf
Marcin Konieczny <marcin_k@coni.pl>
2017-09-20 23:15:51 +0200

    Version 2.0 (z przeciążonym konstruktorem)

--- src/Device.java
+++ src/Device.java
@@ -13,7 +13,12 @@ public class Device {
         this.price = price;
 
     }
-    String getInfo(Device device){
+    public Device(String code, String producer, String type){
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+    }
+    String getInfo(){
         String getinfo = "Kod: "+code+", producent: " + producer +", typ: "+ type +", cena: "+ price;
         return getinfo;
     }
